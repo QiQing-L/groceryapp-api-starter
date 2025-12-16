7188270122e06c5ec0048a34bb8318336450ad5e
Qi Qing <learn.qiqing@gmail.com>
2025-12-16 15:18:12 -0500

    added annotations for RestController, RequestMapping for http://localhost:8080/categories, and CrossOrigin for CategoriesController.
    removed comment for missing getallproducts in ProductsController and comment for getProductsById() placement in CategoriesController, no changes needed.
    
    bug 1 fixed, fixed search() for productDao, String sql is missing maxPrice in filter and the statement minPrice use wrong comparison,

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,6 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -14,6 +12,9 @@ import java.util.List;
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+@RestController
+@RequestMapping("categories")
+@CrossOrigin
 public class CategoriesController
 {
     private CategoryDao categoryDao;
@@ -21,7 +22,6 @@ public class CategoriesController
 
 
     // create an Autowired controller to inject the categoryDao and ProductDao
-
     // add the appropriate annotation for a get action
     public List<Category> getAll()
     {
@@ -38,7 +38,7 @@ public class CategoriesController
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    //?????get product in category??? bug?
+
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
--- src/main/java/org/yearup/controllers/ProductsController.java
+++ src/main/java/org/yearup/controllers/ProductsController.java
@@ -42,8 +42,6 @@ public class ProductsController
         }
     }
 
-    // bug??? missing get all products http://localhost:8080/products.
-
 
     @GetMapping("{id}")
     @PreAuthorize("permitAll()")
